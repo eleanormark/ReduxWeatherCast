8ed09801f08c91f8fb820dab5213fccd9d2fb144
Eleanor Mark <eleanormark@gmail.com>
2017-08-29 01:22:28 -0700

    added smart component search-bar

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_KEY = 'cf50b5bf9f73c662e3194661211919ed';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
