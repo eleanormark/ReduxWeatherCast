bda90fa3ffa13129c3f12007fa80aa77430b1f6f
Eleanor Mark <eleanormark@gmail.com>
2017-08-29 17:29:48 -0700

    added units

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -8,21 +8,15 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
     console.log(temps);
     return (
       <tr key={name}>
-        <td>
-          {name}
-        </td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        {/* <td><GoogleMap lon={lon} lat={lat} /></td> */}
+        <td><Chart data={temps} color="lime" units="K" /></td>
+        <td><Chart data={pressures} color="cyan" units="hPa" /></td>
+        <td><Chart data={humidities} color="pink" units="%" /></td>
       </tr>
     );
   }
@@ -31,10 +25,10 @@ class WeatherList extends Component {
       <table className=" table table-hover">
         <thead>
           <tr>
-            <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+          <th>City</th>
+          <th>Temperature (K)</th>
+          <th>Pressure (hPa)</th>
+          <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
